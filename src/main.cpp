88f929612e127b4e37fcef9700696b1623c268f8
MuhXd <momo2010sofia@gmail.com>
2024-02-27 21:40:08 +0000

    UWU SFX

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,22 @@
 #include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
-
+#include <Geode/ui/GeodeUI.hpp>
 #include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
+bool sfx = true;
+void updsettings() {
+	sfx = Mod::get()->getSettingValue<bool>("SFX");
+};
+class $modify(Uwu, MenuLayer) {
 	bool init() {
 		if (!MenuLayer::init()) {
 			return false;
 		}
+		updsettings();
+	auto geodeButton = dynamic_cast<CCMenuItemSpriteExtra*>(
+		getChildByIDRecursive("geode.loader/geode-button")
+	);
+		geodeButton->m_pfnSelector = menu_selector(Uwu::onGeode);
 		// no ids Yall
 		 for(auto items : CCArrayExt<CCNode*>(this->getChildByIDRecursive("geode.loader/geode-button")->getChildren())) {
 			auto g=items;
@@ -18,10 +27,17 @@ class $modify(MyMenuLayer, MenuLayer) {
 			Spr->setPositionY(icon->getPositionY());
 			g->addChild(Spr);
 			icon->setVisible(false);
+			break;
     	};
 		 }
 
 
 		return true;
+	}
+	void onGeode(CCObject*) {
+		if (sfx) {
+			FMODAudioEngine::sharedEngine()->playEffect("UwU_1.mp3"_spr);
+		}
+		geode::openModsList();
 	}
 };
